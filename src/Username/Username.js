66f15b4540fe4f01692205017308e999a2d70277
kumar <skreddyt@gmail.com>
2019-11-27 10:12:19 -0500

    Update Username.js
    changing the email address

--- src/Username/Username.js
+++ src/Username/Username.js
@@ -6,7 +6,7 @@ const Username = () => {
        
         <p className="ptag">Sravankumar Reddy Tangella</p>
          <p className="ptag">UX/UI Designer</p>
-         <p className="ptag">Reddyreddy7516@gmail.com</p>
+         <p className="ptag">Sravan.tangella</p>
          <p className="ptag">201-381-0835</p>
         </div>
     )}
